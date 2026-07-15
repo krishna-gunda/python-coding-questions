82bbf13b8bba25f604bc14e26d488fcd1f7ca59c
G.Krishna <gundakrishna338@gmail.com>
2026-07-15 15:42:01 +0530

    adding code file

---
+++ Basic_questions/problem_9.py
@@ -0,0 +1,7 @@
+'''Find the sum, difference, product, 
+quotient, floor division, modulus, and exponent of two numbers.'''
+
+
+a=int(input("enter a number"))
+b=int(input("enter b number"))
+print(f'addition is {a+b} and diffrence is {a-b} and quotient is {a/b} and floor division is {a//b} and modules is {a%b} and exponent is {a**b}')
