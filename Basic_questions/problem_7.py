b470d98709de9bc09bfac7773e0d28285ea93047
G.Krishna <gundakrishna338@gmail.com>
2026-07-15 15:29:42 +0530

    adding python file

--- Basic_questions/problem_7.py
+++ Basic_questions/problem_7.py
@@ -17,3 +17,15 @@ print(id(b)) # 1463474835712
 
 # Although a and b have the same contents, they are different objects, so they have different IDs.
 
+
+x = 100
+y = x
+
+print(id(x)) #140717373904408
+
+print(id(y))  #140717373904408
+
+
+# Since x and y reference the same object, both id() calls return the same value.
+
+
---
+++ Basic_questions/problem_8.py
@@ -0,0 +1,14 @@
+'''Check whether an object is an integer using isinstance().'''
+
+
+a=10
+
+print(isinstance(a,int))
+
+
+b=10.0
+
+print(isinstance(b,(int,float)))
+
+
+# isinstance will check whether the object belongs to the predifined classes or not
