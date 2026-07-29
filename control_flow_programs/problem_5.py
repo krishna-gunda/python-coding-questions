e6b4169905b973849d20fbd1f21ca100afb21d9a
G.Krishna <gundakrishna338@gmail.com>
2026-07-29 22:52:06 +0530

    adding problem

--- control_flow_programs/problem_5.py
+++ control_flow_programs/problem_5.py
@@ -1,23 +1,49 @@
-"ARM strong number"
+# "ARM strong number"
 
-"condition the number is equal to the sum of all digits where each digit caluclate power to the length of the numbers"
-
-
-a=153
-result=0
-length=len(str(a))
-for i in range(length):
-    result=result+((a%10)**(length))
-    a=a//10
-print(result)    
+# "condition the number is equal to the sum of all digits where each digit caluclate power to the length of the numbers"
 
 
-def arm(num):
-    result=0
-    length=len(str(num))
-    for i in range(length):
-      result=result+(num%10)**(length)
-      num=num//10
-    return result
+# a=153
+# result=0
+# length=len(str(a))
+# for i in range(length):
+#     result=result+((a%10)**(length))
+#     a=a//10
+# print(result)    
 
-print(arm(153))
+
+# def arm(num):
+#     result=0
+#     length=len(str(num))
+#     for i in range(length):
+#       result=result+(num%10)**(length)
+#       num=num//10
+#     return result
+
+# print(arm(153))
+
+
+
+
+"Write a Python Program to Find Armstrong Number in an Interval."
+
+def armstrong(start,end):
+   lis=[]
+   for i in range(start,end+1):
+      result=0
+      length=len(str(i))
+      for j in range(length):
+         result+=(i%10)**length
+         i=i//10
+      lis.append(result)
+   return lis
+
+
+print(armstrong(100,200))
+
+            
+
+         
+      
+
+
---
+++ control_flow_programs/problem_6.py
@@ -0,0 +1,13 @@
+'calculate to matrix'
+
+import pandas as pd
+
+matrix1=pd.DataFrame([[1,2,3],[4,5,6],[7,8,9]])
+
+matrix2=pd.DataFrame([[9,8,7],[6,5,4],[3,2,1]])
+
+for i in range(len(matrix1)):
+    for j in range(len(matrix2)):
+
+      pass
+    
