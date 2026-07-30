4262b71781cf442b96c47520e4c97ebf05681573
G.Krishna <gundakrishna338@gmail.com>
2026-07-30 11:09:59 +0530

    adding new code

--- control_flow_programs/problem_6.py
+++ control_flow_programs/problem_6.py
@@ -6,8 +6,9 @@ matrix1=pd.DataFrame([[1,2,3],[4,5,6],[7,8,9]])
 
 matrix2=pd.DataFrame([[9,8,7],[6,5,4],[3,2,1]])
 
-for i in range(len(matrix1)):
-    for j in range(len(matrix2)):
+print(matrix1+matrix2)          
+           
+             
 
-      pass
+
     
