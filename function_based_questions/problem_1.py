373169f0f67f6d3ac850daf70016b750598aeab2
G.Krishna <gundakrishna338@gmail.com>
2026-08-05 10:57:45 +0530

    adding function based problems

---
+++ function_based_questions/problem_1.py
@@ -0,0 +1,19 @@
+'Write a function that returns the number 100'
+
+def krishna():
+    return 100
+print(krishna)   #Refers to the function object (its address in memory).
+
+print(krishna())  # here we are calling the function
+
+
+
+
+'Write a function to find the square of a number.'
+
+
+def square(num):
+    return num*num
+
+print(square(5))  # output 25
+print(square(10))  # output 100
