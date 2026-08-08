210a2d7900aa39d31ffbf1be20ae884cab7cd06e
G.Krishna <gundakrishna338@gmail.com>
2026-08-08 12:36:29 +0530

    adding problem2

---
+++ practicing_questions/problem_2.py
@@ -0,0 +1,81 @@
+'''Problem
+
+Employee data is stored as
+
+(Name, Department, Salary)
+
+Print
+
+Highest paid employee
+Lowest paid employee
+Department-wise average salary
+Employees earning above department average
+Sort employees by salary (without using sort() or sorted())
+Input
+5
+
+Krishna AI 85000
+Ram HR 55000
+Rahul AI 92000
+Sita HR 60000
+Anu AI 78000
+Output
+Highest Paid : Rahul
+
+Lowest Paid : Ram
+
+AI Average = 85000
+
+HR Average = 57500
+
+Above Department Average
+
+Rahul
+Sita
+
+Sorted Salaries
+
+Ram
+Sita
+Anu
+Krishna
+Rahul'''
+
+
+
+def store_emply_details(num):
+    data={}
+    for i in range(num):
+        name,dep,sal=input("Enter the details like name,department,salary with space=").split(" ")
+        data[name]=(dep,int(sal))
+    return data
+
+def max_salary(data):
+    max_sal=0
+    max_name=""
+    min_sal=0
+    min_name=""
+    for name in data:
+        if data[name][1]>max_sal:
+          max_sal=data[name][1]
+          max_name=name
+        elif data[name][1]<min_sal and data[name][1]!=max_sal:
+            min_sal=data[name][1]
+            min_name=name
+    return max_name,min_name   
+
+data = {
+    'nikhil': ('AI', 89),
+    'krishna': ('HR', 2345),
+    'vamshi': ('AI', 2389),
+    'sai': ('AI', 389)
+}
+
+#num=int(input("enter the no of employee details to enter in the data base "))
+#data=store_emply_details(num)
+#print(data)
+max_name,min_name=max_salary(data)
+print(max_name)
+print(min_name)
+
+
---
+++ practicing_questions/problem_3.py
@@ -0,0 +1,3 @@
+data={'krishna':('HR',2345),'vamshi':('AI',2389)}
+print(data['krishna'][1])
+print(next(iter(data)))
