ab7c1cc52a1eee131f06f95754d08448a9b8dbcf
G.Krishna <gundakrishna338@gmail.com>
2026-08-13 13:04:43 +0530

    adding files

--- Lambda_based_questions/problem_2.py
+++ Lambda_based_questions/problem_2.py
@@ -1,28 +1,51 @@
-'''Square Every Number in a list'''
+# '''Square Every Number in a list'''
 
-numbers=[1,3,5,6]
-square=map(lambda x:x*x ,numbers)
-print(list(square))
+# numbers=[1,3,5,6]
+# square=map(lambda x:x*x ,numbers)
+# print(list(square))
 
 
-'''Dynamic Input'''
+# '''Dynamic Input'''
 
-numbers=list(map(int,input("Enter the numbers with the space=").split(" ")))
-square=map(lambda x:x*x ,numbers)
-print(list(square))
+# numbers=list(map(int,input("Enter the numbers with the space=").split(" ")))
+# square=map(lambda x:x*x ,numbers)
+# print(list(square))
 
-'''Find Even Numbers'''
+# '''Find Even Numbers'''
 
-numbers=list(map(int,input("Enter the numbers with the space=").split(" ")))
+# numbers=list(map(int,input("Enter the numbers with the space=").split(" ")))
 
-even=filter(lambda x:x%2==0,numbers)
+# even=filter(lambda x:x%2==0,numbers)
 
-print(list(even))
+# print(list(even))
 
-'''Sort Numbers Based on Absolute Value'''
+# '''Sort Numbers Based on Absolute Value'''
 
-number=[-10, 5, -3, 8, -2]
+# number=[-10, 5, -3, 8, -2]
 
-sort=sorted(number,key=lambda x:abs(x))
-print(list(sort))
+# sort=sorted(number,key=lambda x:abs(x))
+# print(sort)
 
+
+'''Sort Tuples by Second Element'''
+
+data = [
+    ("A", 50),
+    ("B", 20),
+    ("C", 80),
+    ("D", 40)
+]
+
+tuple_sort=sorted(data,key=lambda x:x[1],reverse=True) #descending order
+print(tuple_sort)
+
+
+students = [
+    {"name": "Krishna", "marks": 85},
+    {"name": "Rahul", "marks": 72},
+    {"name": "Anil", "marks": 95},
+    {"name": "Sita", "marks": 68}
+]
+dict_sort=sorted(students,key=lambda x:x['marks'])
+
+print(dict_sort)
