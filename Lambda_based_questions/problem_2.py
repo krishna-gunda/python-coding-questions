500cdf51ad094b6aa7b02f3fc4f860c7b3cb50e5
G.Krishna <gundakrishna338@gmail.com>
2026-08-13 12:20:51 +0530

    adding lambda questions

--- Lambda_based_questions/problem_2.py
+++ Lambda_based_questions/problem_2.py
@@ -1,23 +1,23 @@
 '''Square Every Number in a list'''
 
-# numbers=[1,3,5,6]
-# square=map(lambda x:x*x ,numbers)
-# print(list(square))
+numbers=[1,3,5,6]
+square=map(lambda x:x*x ,numbers)
+print(list(square))
 
 
 '''Dynamic Input'''
 
-# numbers=list(map(int,input("Enter the numbers with the space=").split(" ")))
-# square=map(lambda x:x*x ,numbers)
-# print(list(square))
+numbers=list(map(int,input("Enter the numbers with the space=").split(" ")))
+square=map(lambda x:x*x ,numbers)
+print(list(square))
 
 '''Find Even Numbers'''
 
-# numbers=list(map(int,input("Enter the numbers with the space=").split(" ")))
+numbers=list(map(int,input("Enter the numbers with the space=").split(" ")))
 
-# even=filter(lambda x:x%2==0,numbers)
+even=filter(lambda x:x%2==0,numbers)
 
-# print(list(even))
+print(list(even))
 
 '''Sort Numbers Based on Absolute Value'''
 
