390cb298330b6a4fe991ae5fc1ce42d25e732357
G.Krishna <gundakrishna338@gmail.com>
2026-08-17 15:54:06 +0530

    adding the updated code

--- function_based_questions/problem_6.py
+++ function_based_questions/problem_6.py
@@ -4,16 +4,34 @@ repeating (1,2,3...9,1,2,3...), for n rows where row i has i numbers. However, s
 (use continue) for any row number that's a multiple of 3, and stop the whole pyramid (using a flag + 
 break, no early-return) if any row would exceed 15 numbers.'''
 
-n=int(input("Enter a number="))
-num = 1
+n = int(input("Enter a number = "))
 
-for i in range(1, n+1):
-    if i%3==0:
+num = 1
+stop = False
+
+for i in range(1, n + 1):
+
+    # Stop if the row would exceed 15 numbers
+    if i > 15:
+        stop = True
+        break
+
+    # Skip rows that are multiples of 3
+    if i % 3 == 0:
         continue
+
+    # Print i numbers in the row
     for j in range(i):
         print(num, end=" ")
+
         num += 1
-        if num==10:
-            num=1
+
+        # Restart from 1 after 9
+        if num == 10:
+            num = 1
+
     print()
-    
+
+    # Check the flag
+    if stop:
+        break
