4f3481964c0cf58a1c7d5b6b97a345f1971708be
G.Krishna <gundakrishna338@gmail.com>
2026-08-24 10:35:00 +0530

    adding the polymorphism

---
+++ Polymorphism/Explanation_.py
@@ -0,0 +1,5 @@
+# polymorphism
+
+# polymorphism is the technique that we can use the same method names for the diffrent classes
+# in the diffrent classes we can use the same method name and the methods would perform diffrent actions based on the class
+# when ever we call the child class method the child method would only execute
